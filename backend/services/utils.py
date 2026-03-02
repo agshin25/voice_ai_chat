e38ec752323d349a8e1b418e03033d306ef3f657
zahid022 <zahid.hzade001@gmail.com>
2026-03-02 10:50:07 -0600

    new

--- backend/services/utils.py
+++ backend/services/utils.py
@@ -1,6 +1,7 @@
 import tempfile
 
-def save_audio(audio_bytes: bytes, suffix: str = ".wav") -> str:                                                                                                                                              tmp = tempfile.NamedTemporaryFile(delete=False, suffix=suffix)                                                                                                                                      
+def save_audio(audio_bytes: bytes, suffix: str = ".wav") -> str:
+    tmp = tempfile.NamedTemporaryFile(delete=False, suffix=suffix)
     tmp.write(audio_bytes)
     tmp.close()
     return tmp.name
